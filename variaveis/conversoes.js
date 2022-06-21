ba6eb625ce7422c504eac169844381302e43ecb2
Maycon da Fontoura Ramon <maycon@pop-os.localdomain>
2022-06-21 20:11:33 -0300

    códigos var e conversões

---
+++ variaveis/conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão implícia
+
+
+// conversão explícita
+const numero = 456;
+const numeroString = Number("456a");
+
+console.log(numero + Number(numeroString))
+
+
